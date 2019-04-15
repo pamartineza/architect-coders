7acb8cdaefa214f563908bc82eba47eaf16da604
Antonio Leiva <antonioleivag@gmail.com>
2019-04-15 17:27:50 +0200

    Extract detailed info from DetailActivity to a custom View

--- app/src/main/java/com/antonioleiva/mymovies/ui/DetailActivity.kt
+++ app/src/main/java/com/antonioleiva/mymovies/ui/DetailActivity.kt
@@ -3,8 +3,6 @@ package com.antonioleiva.mymovies.ui
 import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.text.bold
-import androidx.core.text.buildSpannedString
 import com.antonioleiva.mymovies.R
 import com.antonioleiva.mymovies.model.Movie
 import kotlinx.android.synthetic.main.activity_detail.*
@@ -23,26 +21,8 @@ class DetailActivity : AppCompatActivity() {
         with(intent.getParcelableExtra<Movie>(MOVIE)) {
             movieDetailToolbar.title = title
             movieDetailImage.loadUrl("https://image.tmdb.org/t/p/w780$backdropPath")
-
             movieDetailSummary.text = overview
-
-            movieDetailInfo.text = buildSpannedString {
-
-                bold { append("Original language: ") }
-                appendln(originalLanguage)
-
-                bold { append("Original title: ") }
-                appendln(originalTitle)
-
-                bold { append("Release date: ") }
-                appendln(releaseDate)
-
-                bold { append("Popularity: ") }
-                appendln(popularity.toString())
-
-                bold { append("Vote Average: ") }
-                append(voteAverage.toString())
-            }
+            movieDetailInfo.setMovie(this)
         }
     }
 }
