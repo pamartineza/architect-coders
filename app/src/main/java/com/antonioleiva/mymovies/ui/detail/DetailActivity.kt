76ec620b5439d9d1f32cc884d12fe713a7b38d6d
Antonio Leiva <antonioleivag@gmail.com>
2019-04-16 19:08:39 +0200

    Convert to MVP

--- app/src/main/java/com/antonioleiva/mymovies/ui/detail/DetailActivity.kt
+++ app/src/main/java/com/antonioleiva/mymovies/ui/detail/DetailActivity.kt
@@ -8,22 +8,30 @@ import com.antonioleiva.mymovies.model.Movie
 import com.antonioleiva.mymovies.ui.common.loadUrl
 import kotlinx.android.synthetic.main.activity_detail.*
 
-class DetailActivity : AppCompatActivity() {
+class DetailActivity : AppCompatActivity(), DetailPresenter.View {
 
     companion object {
         const val MOVIE = "DetailActivity:movie"
     }
 
+    private val presenter = DetailPresenter()
+
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
+        presenter.onCreate(this, intent.getParcelableExtra(MOVIE))
+    }
 
-        with(intent.getParcelableExtra<Movie>(MOVIE)) {
-            movieDetailToolbar.title = title
-            movieDetailImage.loadUrl("https://image.tmdb.org/t/p/w780$backdropPath")
-            movieDetailSummary.text = overview
-            movieDetailInfo.setMovie(this)
-        }
+    override fun onDestroy() {
+        presenter.onDestroy()
+        super.onDestroy()
+    }
+
+    override fun updateUI(movie: Movie) = with(movie) {
+        movieDetailToolbar.title = title
+        movieDetailImage.loadUrl("https://image.tmdb.org/t/p/w780$backdropPath")
+        movieDetailSummary.text = overview
+        movieDetailInfo.setMovie(this)
     }
 }
